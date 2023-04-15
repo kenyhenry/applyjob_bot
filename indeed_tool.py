95857fa48ccfe33fe668fb103620ecb4b899f4c4
kény Henry <henry.keny@outlook.fr>
2023-04-15 00:41:26 -0400

    navigate into page of indeed

--- indeed_tool.py
+++ indeed_tool.py
@@ -18,7 +18,7 @@ apply_nb = 0
 page = 1
 offer_nb = 1
 
-f = open('not_applied.txt', 'a')
+# f = open('not_applied.txt', 'a')
 
 indeed_home = "https://fr.indeed.com/jobs?q=nodejs&l=France&vjk=e6c0ee2350ed43f0"
 
@@ -65,4 +65,22 @@ for offer in offers:
     # /!\ web site detect scraping bot if each action is seperate by same time /!\
     time.sleep(randrange(5))
 
-# end page | next page
+# go to the next page
+
+# retrive nav element list
+nav_list = driver.find_elements(by=By.TAG_NAME, value="nav")
+for nav in nav_list:
+    # if element is navigation bar
+    if nav.get_attribute("class") == "css-jbuxu0 ecydgvn0":
+        next_page_list = nav.find_elements(by=By.XPATH, value="./child::*")
+        # retrieve child of nav list
+        for e in next_page_list:
+            a_list = e.find_elements(by=By.XPATH, value="./child::*")
+            # iterate on element in each nav button
+            for a in a_list:
+                # if element is next page
+                if a.get_attribute("aria-label"):
+                    if a.get_attribute("aria-label") == "Next Page":
+                        # open link of element
+                        next_page = a.get_attribute("href")
+                        driver.get(next_page)
